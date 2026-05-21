2d61b09ea0c5ab287e0672f8d19125098f12e6ec
George Marcone <gmarcone@gmail.com>
2026-05-21 16:58:15 -0300

    renomeio de diretório

---
+++ Models/Aluno.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace form_API.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+        public string DataNasc { get; set; } = string.Empty;
+        public int ProfessorId { get; set; }
+        public Professor? Professor { get; set; }
+    }
+}
